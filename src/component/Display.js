a2a53084f2ee406ab6d307d64aa29e4eea4cafa8
Krishna Prasad Kesani <kkp699@gmail.com>
2021-11-03 13:20:10 -0700

    set display color based on the viewport width

--- src/component/Display.js
+++ src/component/Display.js
@@ -8,9 +8,22 @@ export default class Display extends React.Component {
     value: PropTypes.string,
   };
 
+  state = {
+    width: 0,
+  };
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
   render() {
     return (
-      <div className="component-display">
+      <div className={this.state.width > 500 ? "component-display" : "component-display-red"}>
         <div>{this.props.value}</div>
       </div>
     );
